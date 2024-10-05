10468269764d2fbd6d4d508062e672fffab8b7bc
Vincent G <125322121+VincentG03@users.noreply.github.com>
2024-10-05 14:44:34 +1000

    Initial commit

--- map_chart.js
+++ map_chart.js
@@ -1,5 +1,5 @@
 var vg_1 = "map.vg.json";
-vegaEmbed("#map_graph", vg_1).then(function(result) {
+vegaEmbed("#choropleth", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
