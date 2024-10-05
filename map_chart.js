801413493ccd4ad6c0be6e21fbb30bce5511fe1e
Vincent G <125322121+VincentG03@users.noreply.github.com>
2024-10-05 19:01:46 +1000

    Finalised graphs

--- map_chart.js
+++ map_chart.js
@@ -1,3 +1,7 @@
 var vg_1 = "choropleth.vg.json";
 vegaEmbed("#choropleth", vg_1).then(function(result) {
 }).catch(console.error);
+
+var vg_1 = "bar.vg.json";
+vegaEmbed("#bar", vg_1).then(function(result) {
+}).catch(console.error);
