d12e1d69275c752035786711eec14fb9f886b09b
Vincent G <125322121+VincentG03@users.noreply.github.com>
2024-10-05 16:28:43 +1000

    More files

--- map_chart.js
+++ map_chart.js
@@ -1,5 +1,3 @@
-var vg_1 = "map.vg.json";
+var vg_1 = "choropleth.vg.json";
 vegaEmbed("#choropleth", vg_1).then(function(result) {
-// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-
